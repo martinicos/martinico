e463a096cea7e41c4482d1110ee4e03ad342c553
martinicos <127124676+martinicos@users.noreply.github.com>
2023-03-08 09:29:31 -0300

    Update rdm.js

--- rdm.js
+++ rdm.js
@@ -1,3 +1,10 @@
-function generarNumeroAleatorio(minimo, maximo) {
-  return Math.floor(Math.random() * (maximo - minimo + 1)) + minimo;
-}
+// Obtener el botón por su ID
+const myButton = document.getElementById("myButton");
+
+// Agregar un listener de clic al botón
+myButton.addEventListener("click", function() {
+  // Aquí se puede agregar cualquier código que se desee que se ejecute cuando se haga clic en el botón
+  
+  // Mostrar un mensaje de alerta con información adicional
+  alert("¡Has hecho clic en el botón! Aquí hay información adicional que podría ser de ayuda.");
+});
