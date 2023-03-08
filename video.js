adb5e574200a02de6a87ef69996b8276c77f1dfb
martinicos <127124676+martinicos@users.noreply.github.com>
2023-03-08 09:48:06 -0300

    Create video.js

---
+++ video.js
@@ -0,0 +1,4 @@
+const botonVideoIntroductorio = document.getElementById("video-introductorio");
+botonVideoIntroductorio.addEventListener("click", () => {
+  window.location.href = "https://www.ejemplo.com/video-introductorio";
+});
