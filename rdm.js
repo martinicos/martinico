06b4714daebd8a1314babffafce29889a47b378e
martinicos <127124676+martinicos@users.noreply.github.com>
2023-03-08 09:40:53 -0300

    Update rdm.js

--- rdm.js
+++ rdm.js
@@ -1,7 +1,43 @@
-
-var imagen = document.getElementById("mi-imagen");
-var informacion = document.getElementById("informacion");
-
-imagen.addEventListener("click", function() {
-  informacion.innerHTML = "mail: hermida.martin@gmail.com";
-});
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Mi Currículum Vitae</title>
+    <style>
+      /* Agrega estilos CSS aquí */
+    </style>
+  </head>
+  <body>
+    <h1>Mi Currículum Vitae</h1>
+    
+    <h2>Datos personales</h2>
+    <p>Nombre: <span id="nombre"></span></p>
+    <p>Correo electrónico: <span id="correo"></span></p>
+    <p>Teléfono: <span id="telefono"></span></p>
+    
+    <h2>Educación</h2>
+    <ul id="educacion">
+      <li>
+        <p><strong>Título:</strong> <span></span></p>
+        <p><strong>Institución:</strong> <span></span></p>
+        <p><strong>Año de graduación:</strong> <span></span></p>
+      </li>
+    </ul>
+    
+    <h2>Experiencia laboral</h2>
+    <ul id="experiencia">
+      <li>
+        <p><strong>Puesto:</strong> <span></span></p>
+        <p><strong>Empresa:</strong> <span></span></p>
+        <p><strong>Fecha de inicio:</strong> <span></span></p>
+        <p><strong>Fecha de finalización:</strong> <span></span></p>
+        <p><strong>Descripción:</strong> <span></span></p>
+      </li>
+    </ul>
+    
+    <h2>Habilidades</h2>
+    <ul id="habilidades">
+      <li><span></span></li>
+    </ul>
+    
+    <script>
