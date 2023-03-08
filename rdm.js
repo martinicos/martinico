3f4c3fc063c52b8ee0b94dfea2006313ed1dda0c
martinicos <127124676+martinicos@users.noreply.github.com>
2023-03-08 09:36:47 -0300

    Update rdm.js

--- rdm.js
+++ rdm.js
@@ -1,8 +1,7 @@
-// Obtiene la referencia a la imagen y al elemento donde se mostrará la información
+
 var imagen = document.getElementById("mi-imagen");
 var informacion = document.getElementById("informacion");
 
-// Escucha el evento "clic" en la imagen y muestra la información
 imagen.addEventListener("click", function() {
   informacion.innerHTML = "mail: hermida.martin@gmail.com";
 });
